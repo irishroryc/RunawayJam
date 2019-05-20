20068b2b31755b2925a16b2d3b269aa2db8948ae
Rory Connolly <roryc@Rorys-MacBook-Pro.local>
2019-05-20 11:46:46 -0400

    Update code to use webhook for messaging

--- jam.py
+++ jam.py
@@ -13,6 +13,7 @@ PHISHIN_LOGO = "http://phish.in/assets/logo-text-dcfa821a4529a5e9e377dd8f6f1a6b1
 
 song_map = {}
 
+# VAlidating requests to the 
 def is_request_valid(request):
     is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
     is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
@@ -21,10 +22,13 @@ def is_request_valid(request):
         is_token_valid = request.form['token'] == os.environ['VISCO_TOKEN']
         is_team_id_valid = request.form['team_id'] == os.environ['VISCO_TEAM_ID']
 
-    print("token_valid = ",is_token_valid)
-    print("team_valid = ",is_team_id_valid)
-
     return is_token_valid and is_team_id_valid
+
+def get_random_song():
+    all_req = requests.get(url="https://api.phish.net/v3/jamcharts/all?apikey="+DOT_NET_API_KEY)
+    all_dict = all_req.json()
+    random_song = random.choice(list(all_dict['response']['data']))
+    return random_song['link'].split('/')[-1],random_song['song']
 
 def update_song_map():
     all_req = requests.get(url="https://api.phish.net/v3/jamcharts/all?apikey="+DOT_NET_API_KEY)
@@ -36,7 +40,6 @@ def update_song_map():
 def get_jam_date(song_id):
     jam_url = "https://api.phish.net/v3/jamcharts/get?songid="+str(song_id)+"&apikey="+DOT_NET_API_KEY
     jam_req = requests.get(url=jam_url)
-    #print("DEBUG: ",jam_req.json())
     jam_req_dict = jam_req.json()['response']['data']['entries']
     random_choice = random.randint(0,len(jam_req_dict))
     return jam_req_dict[random_choice]['showdate']
@@ -55,32 +58,36 @@ def jam():
     if request.form['text']:
         PHISHIN_SONG = request.form['text']
         CLEAN_SONG = PHISHIN_SONG.replace(' ','-').lower()
-        PHISHIN_URL = "http://phish.in/api/v1/songs/"+CLEAN_SONG+".json"
     else:
-        return jsonify(
-            response_type='in_channel',
-            text="Sorry, couldn't find that song. Go Phish!",
-        )
+        CLEAN_SONG,PHISHIN_SONG = get_random_song()
 
-    #print("PHISHIN_URL = ",PHISHIN_URL)
-    #print("PHISHIN_HEADERS = ",PHISHIN_HEADERS)
+    PHISHIN_URL = "http://phish.in/api/v1/songs/"+CLEAN_SONG+".json"
+
     r = requests.get(url = PHISHIN_URL, headers=PHISHIN_HEADERS)
 
     if r.status_code == 404:
-        return jsonify(
-            response_type='in_channel',
-            text="Sorry, couldn't find that song. Go Phish!",
-        )
+        payload_data = {
+            'response_type':'in_channel',
+            'text':"Sorry, couldn't find that song.  Go Phish!"
+        }
+        posted = requests.post(request.form['response_url'],
+                               json=payload_data,
+                               headers={'Content-Type': 'application/json'})
+        return '',200
 
     update_song_map()
 
     if CLEAN_SONG in song_map:
         jam_song_id = song_map[CLEAN_SONG]
     else:
-        return jsonify(
-            response_type='in_channel',
-            text="No jamchart entry for "+PHISHIN_SONG+", that song suuucks! (Go Phish!)",
-        ) 
+        payload_data = {
+            'response_type':'in_channel',
+            'text':"Sorry, no jamchart versions of "+PHISHIN_SONG+" exist. Go Phish!"
+        }
+        posted = requests.post(request.form['response_url'],
+                               json=payload_data,
+                               headers={'Content-Type': 'application/json'})
+        return '',200
 
     jam_date = get_jam_date(jam_song_id)
 
@@ -93,6 +100,7 @@ def jam():
     for jam in jam_tracks:
         if jam['show_date'] == jam_date:
             track = jam
+            break
     #print("jam_track =",track)
     track_mp3 = track['mp3']
     track_date = track['show_date']
@@ -100,16 +108,21 @@ def jam():
     track_venue = get_venue(track['show_id'])
     track_title = track_venue+"\n"+track_date
     track_link = "http://phish.in/"+track_date+"/"+CLEAN_SONG
-    track_pretext = "Random Jamchart version of "+PHISHIN_SONG+" from "+track_venue
+    track_pretext = "Feast your ears on this "+track_set+" "+PHISHIN_SONG+" from "+track_venue
     track_notes = None
     for tags in track['tags']:
         if tags['notes'] is not None:
             track_notes = tags['notes']
+            break
     if track_notes == None:
         track_notes = "Check out this killer "+track_set+" "+PHISHIN_SONG+"!!"
 
-    return jsonify(
-        response_type='in_channel',
-        text=track_pretext,
-        attachments=[{'pretext':track_notes,'image_url':PHISHIN_LOGO,'title':track_title,'title_link':track_link}]
-    )
+    payload_data = {
+        'response_type':'in_channel',
+        'text':track_pretext,
+        'attachments':[{'pretext':track_notes,'image_url':PHISHIN_LOGO,'title':track_title,'title_link':track_link}]
+    }
+
+    posted = requests.post(request.form['response_url'],json=payload_data,headers={'Content-Type': 'application/json'})
+
+    return '',200
